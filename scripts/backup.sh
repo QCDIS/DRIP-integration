499c7c47cc668a723d5f39e76d0235523d6b1351
skoulouzis <S.Koulouzis@uva.nl>
2017-10-02 13:23:33 +0200

    remove sudo

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -11,19 +11,19 @@ year=`date '+%Y'`
 
 
 # Manager logs 
-sudo docker cp dockerfiles_drip-manager_1:$catalina_home/logs $backup_folder
+docker cp dockerfiles_drip-manager_1:$catalina_home/logs $backup_folder
 cd $backup_folder/logs
-sudo tar -zcvf DRIP_logs-$month-$year.tar.gz *.$year-$month-*.*
-sudo mv DRIP_logs-10-2017.tar.gz $backup_folder
-sudo rm $backup_folder/logs/*.$year-$month-*.*
-sudo docker exec -it dockerfiles_drip-manager_1 sh -c "rm $catalina_home/logs/*.$year-$month-*.*"
+tar -zcvf DRIP_logs-$month-$year.tar.gz *.$year-$month-*.*
+mv DRIP_logs-10-2017.tar.gz $backup_folder
+rm $backup_folder/logs/*.$year-$month-*.*
+docker exec -it dockerfiles_drip-manager_1 sh -c "rm $catalina_home/logs/*.$year-$month-*.*"
 
 
 
 # Mogno
-sudo docker exec -it dockerfiles_mongo_1 sh -c "mongodump -v --host localhost:27017 --out=/tmp"
-sudo docker cp dockerfiles_mongo_1:/tmp/drip $backup_folder
+docker exec -it dockerfiles_mongo_1 sh -c "mongodump -v --host localhost:27017 --out=/tmp"
+docker cp dockerfiles_mongo_1:/tmp/drip $backup_folder
 cd $backup_folder/drip
-sudo tar -zcvf $backup_folder/DRIP_DB-$month-$year.tar.gz *.*
-sudo rm  $backup_folder/drip/*
-sudo docker exec -it dockerfiles_mongo_1 sh -c "rm -r /tmp/drip"
+tar -zcvf $backup_folder/DRIP_DB-$month-$year.tar.gz *.*
+rm  $backup_folder/drip/*
+docker exec -it dockerfiles_mongo_1 sh -c "rm -r /tmp/drip"
