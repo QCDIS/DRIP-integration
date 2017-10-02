6cb8e928437d7d16b9a1886f772a490104581dab
skoulouzis <S.Koulouzis@uva.nl>
2017-10-02 14:48:46 +0200

    chmod +x

--- scripts/restore_mongo.sh
+++ scripts/restore_mongo.sh
@@ -10,3 +10,4 @@ mkdir $backup_folder/drip_db
 tar -xzvf $backup_folder/DRIP_DB-$year-$month-$day.tar.gz -C $backup_folder/drip_db
 docker cp $backup_folder/drip_db dockerfiles_mongo_1:/tmp/drip_db
 docker exec -it dockerfiles_mongo_1 sh -c "mongorestore --db drip --drop /tmp/drip_db"
+docker exec -it dockerfiles_mongo_1 sh -c "rm -r /tmp/drip_db"
