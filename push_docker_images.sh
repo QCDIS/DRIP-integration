3a4f7cca2b469faa8cf01d69add1b1343529f967
skoulouzis <S.Koulouzis@uva.nl>
2019-09-30 14:16:14 +0200

    push images to repo

--- push_docker_images.sh
+++ push_docker_images.sh
@@ -3,19 +3,26 @@
 cd Dockerfiles/deployer
 docker build -t drip-deployer .
 docker tag drip-deployer alogo53/drip-deployer:$1
+docker push alogo53/drip-deployer:$1
 
 cd ../manager
 docker build -t drip-manager .
 docker tag drip-manager alogo53/drip-manager:$1
+docker push alogo53/drip-manager:$1
 
 cd ../parser
 docker build -t drip-parser .
 docker tag drip-parser alogo53/drip-parser:$1
+docker push alogo53/drip-parser:$1
 
 cd ../planner
 docker build -t drip-planner .
 docker tag drip-planner alogo53/drip-planner:$1
+docker push logo53/drip-planner:$1
 
 cd ../provisioner
 docker build -t drip-provisioner .
 docker tag drip-provisioner alogo53/drip-provisioner:$1
+docker push alogo53/drip-provisioner:$1
+
+
